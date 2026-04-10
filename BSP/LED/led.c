0c137a033f13b6b2343ad30e8c6dad7c54f98804
haijiek <1653001767@qq.com>
2026-04-10 12:47:20 +0800

    LED

--- BSP/LED/led.c
+++ BSP/LED/led.c
@@ -1,3 +1,26 @@
 #include "led.h"
+#include "main.h"
+
+// 初始化LED GPIO
+void LED_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    // 配置LED0 (PF9)
+    GPIO_InitStruct.Pin = LED0_GPIO_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LED0_GPIO_PORT, &GPIO_InitStruct);
+
+    // 配置LED1 (PF10)
+    GPIO_InitStruct.Pin = LED1_GPIO_PIN;
+    HAL_GPIO_Init(LED1_GPIO_PORT, &GPIO_InitStruct);
+
+    // 初始状态：关闭两个LED
+    LED0_OFF();
+    LED1_OFF();
+}
+#include "led.h"
 
 
